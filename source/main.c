c3ea94bb9bb755df7568a29b323266017f39ef33
rabdiego <diegorabelodesa@gmail.com>
2025-11-22 17:43:18 -0300

    Snake now can get bigger

--- source/main.c
+++ source/main.c
@@ -8,9 +8,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include "sheet.h"
 #include "bg.h"
+
+#define MAX_SNAKE_LENGTH 127
 
 typedef struct
 {
@@ -19,6 +22,19 @@ typedef struct
     u16 attr2;
     u16 pad;
 } OBJ_ATTR;
+
+typedef struct
+{
+    int x;
+    int y;
+    int direction;
+} Position;
+
+int generateRandomTilePos(int max)
+{
+    int range = max + 1;
+    return (rand() % range) * 16;
+}
 
 void initBackground(void)
 {
@@ -48,6 +64,8 @@ void initBackground(void)
 
 int main(void)
 {
+    srand(time(NULL));
+
     irqInit();
     irqEnable(IRQ_VBLANK);
 
@@ -63,9 +81,6 @@ int main(void)
         OAM[i].attr2 = 0;
     }
 
-    int x = 96;
-    int y = 64;
-
     int upIndex = 0;
     int downIndex = 4;
     int leftIndex = 16;
@@ -74,10 +89,17 @@ int main(void)
     int pumpkinIndex = 36;
     int bobIndex = 48;
 
-    int movingDirection = 2;
+    int tailDirection = 2;
     int currentSpriteIndex = leftIndex;
 
     int timer = 0;
+
+    Position body[MAX_SNAKE_LENGTH];
+    int snakeLength = 1;
+    body[0].x = 96; body[0].y = 64; body[0].direction = 2;
+
+    Position bob;
+    bob.x = 144; bob.y = 80; bob.direction = 0;
 
     initBackground();
     
@@ -88,50 +110,113 @@ int main(void)
 
         timer++;
 
-        if (keysHeld() & KEY_UP)    movingDirection = 0;
-        if (keysHeld() & KEY_DOWN)  movingDirection = 1;
-        if (keysHeld() & KEY_LEFT)  movingDirection = 2;
-        if (keysHeld() & KEY_RIGHT) movingDirection = 3;
+        if (keysHeld() & KEY_UP)    body[0].direction = 0;
+        if (keysHeld() & KEY_DOWN)  body[0].direction = 1;
+        if (keysHeld() & KEY_LEFT)  body[0].direction = 2;
+        if (keysHeld() & KEY_RIGHT) body[0].direction = 3;
+        
+        tailDirection = body[snakeLength-1].direction;
 
         if (timer % 10 == 0)
         {
+            for (int i = snakeLength - 1; i > 0; i--)
+            {
+                body[i].x = body[i-1].x;
+                body[i].y = body[i-1].y;
+                body[i].direction = body[i-1].direction;
+            }
         
-            switch (movingDirection)
+            switch (body[0].direction)
             {
                 case 0:
                     currentSpriteIndex = upIndex;
-                    y -= 16;
+                    body[0].y -= 16;
                     break;
 
                 case 1:
                     currentSpriteIndex = downIndex;
-                    y += 16;
+                    body[0].y += 16;
                     break;
                 
                 case 2:
                     currentSpriteIndex = leftIndex;
-                    x -= 16;
+                    body[0].x -= 16;
                     break;
 
                 case 3:
                     currentSpriteIndex = rightIndex;
-                    x += 16;
+                    body[0].x += 16;
                     break;
                 
                 default:
-                    currentSpriteIndex = upIndex;
                     break;
-            }    
+            }
+
+            if (body[0].x == bob.x && body[0].y == bob.y)
+            {
+                switch (tailDirection)
+                {
+                    case 0:
+                        body[snakeLength].x = body[snakeLength-1].x;
+                        body[snakeLength].y = body[snakeLength-1].y + 16;
+                        break;
+                    
+                    case 1:
+                        body[snakeLength].x = body[snakeLength-1].x;
+                        body[snakeLength].y = body[snakeLength-1].y - 16;
+                        break;
+
+                    case 2:
+                        body[snakeLength].x = body[snakeLength-1].x + 16;
+                        body[snakeLength].y = body[snakeLength-1].y;
+                        break;
+
+                    case 3:
+                        body[snakeLength].x = body[snakeLength-1].x - 16;
+                        body[snakeLength].y = body[snakeLength-1].y;
+                        break;
+
+                    default:
+                        break;
+                }
+                
+                body[snakeLength].direction = tailDirection;
+                snakeLength++;
+
+                int bobIsOutSnake = 0;
+                while (!bobIsOutSnake)
+                {
+                    bob.x = generateRandomTilePos(14);
+                    bob.y = generateRandomTilePos(9);
+
+                    bobIsOutSnake = 1;
+                    for (int i = 0; i < snakeLength; i++)
+                    {
+                        if (bob.x == body[i].x && bob.y == body[i].y)
+                        {
+                            bobIsOutSnake = 0;
+                        }
+                    }
+                }
+            }
         }
 
         // Player
-        OAM[0].attr0 = (y & 0xFF) | ATTR0_SQUARE | ATTR0_COLOR_16;
-        OAM[0].attr1 = (x & 0x1FF) | ATTR1_SIZE_16;
+        OAM[0].attr0 = (body[0].y & 0xFF) | ATTR0_SQUARE | ATTR0_COLOR_16;
+        OAM[0].attr1 = (body[0].x & 0x1FF) | ATTR1_SIZE_16;
         OAM[0].attr2 = currentSpriteIndex | ATTR2_PALETTE(0);
 
+        // Body
+        for (int i = 1; i < snakeLength; i++)
+        {
+            OAM[i].attr0 = (body[i].y & 0xFF) | ATTR0_SQUARE | ATTR0_COLOR_16;
+            OAM[i].attr1 = (body[i].x & 0x1FF) | ATTR1_SIZE_16;
+            OAM[i].attr2 = bodyIndex | ATTR2_PALETTE(0);
+        }
+
         // Bob
-        OAM[1].attr0 = (80 & 0xFF) | ATTR0_SQUARE | ATTR0_COLOR_16;
-        OAM[1].attr1 = (144 & 0x1FF) | ATTR1_SIZE_16;
-        OAM[1].attr2 = bobIndex | ATTR2_PALETTE(0);
+        OAM[127].attr0 = (bob.y & 0xFF) | ATTR0_SQUARE | ATTR0_COLOR_16;
+        OAM[127].attr1 = (bob.x & 0x1FF) | ATTR1_SIZE_16;
+        OAM[127].attr2 = bobIndex | ATTR2_PALETTE(0);
     }
 }
