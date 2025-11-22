d73374201e0131160817c0e454561e60990cb1ef
rabdiego <diegorabelodesa@gmail.com>
2025-11-22 18:41:58 -0300

    Playable game

---
+++ source/font.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(font)
+
+//======================================================================
+//
+//	font, 128x48@4, 
+//	+ palette 256 entries, not compressed
+//	+ 96 tiles not compressed
+//	Total size: 512 + 3072 = 3584
+//
+//	Time-stamp: 2025-11-22, 21:17:36
+//	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_FONT_H
+#define GRIT_FONT_H
+
+#define fontTilesLen 3072
+extern const unsigned int fontTiles[768];
+
+#define fontPalLen 512
+extern const unsigned short fontPal[256];
+
+#endif // GRIT_FONT_H
+
+//}}BLOCK(font)
--- source/main.c
+++ source/main.c
@@ -12,6 +12,7 @@
 
 #include "sheet.h"
 #include "bg.h"
+#include "font.h"
 
 #define MAX_SNAKE_LENGTH 127
 
@@ -33,12 +34,12 @@ typedef struct
 int generateRandomTilePos(int max)
 {
     int range = max + 1;
-    return (rand() % range) * 16;
+    return (rand() % range);
 }
 
 void initBackground(void)
 {
-    REG_BG0CNT = BG_SIZE_0 | SCREEN_BASE(31) | CHAR_BASE(0);
+    REG_BG0CNT = BG_SIZE_0 | SCREEN_BASE(31) | CHAR_BASE(0) | BG_PRIORITY(2);
 
     memcpy(CHAR_BASE_BLOCK(0), bgTiles, bgTilesLen);
     memcpy(BG_PALETTE, bgPal, bgPalLen);
@@ -62,6 +63,20 @@ void initBackground(void)
     }
 }
 
+void drawText(int x, int y, const char *text)
+{
+    u16 *map = SCREEN_BASE_BLOCK(30);
+
+    int i = 0;
+    while (text[i] != '\0')
+    {
+        int tileID = text[i] - 32;
+        map[y*32 + x + i] = tileID | (1 << 12);
+
+        i++;
+    }
+}
+
 int main(void)
 {
     srand(time(NULL));
@@ -69,10 +84,16 @@ int main(void)
     irqInit();
     irqEnable(IRQ_VBLANK);
 
-    SetMode(MODE_0 | OBJ_ON | BG0_ON | OBJ_1D_MAP);
+    SetMode(MODE_0 | BG0_ON | BG1_ON | OBJ_ON | OBJ_1D_MAP);
+
+    REG_BG1CNT = BG_SIZE_0 | SCREEN_BASE(30) | CHAR_BASE(1) | BG_PRIORITY(0);
+    dmaCopy(fontTiles, CHAR_BASE_BLOCK(1), fontTilesLen);
+    dmaCopy(fontPal, BG_PALETTE + 16, fontPalLen);
 
     dmaCopy(sheetPal, SPRITE_PALETTE, sheetPalLen);
     dmaCopy(sheetTiles, SPRITE_GFX, sheetTilesLen);
+
+    char buffer[30];
 
     for(int i = 0; i < 128; i++)
     {
@@ -86,13 +107,13 @@ int main(void)
     int leftIndex = 16;
     int rightIndex = 20;
     int bodyIndex = 32;
-    int pumpkinIndex = 36;
-    int bobIndex = 48;
 
     int tailDirection = 2;
     int currentSpriteIndex = leftIndex;
+    int bobSpriteIndex = 48;
 
     int timer = 0;
+    int maxScore = 0;
 
     Position body[MAX_SNAKE_LENGTH];
     int snakeLength = 1;
@@ -100,6 +121,8 @@ int main(void)
 
     Position bob;
     bob.x = 144; bob.y = 80; bob.direction = 0;
+
+    int isGameOver = 0;
 
     initBackground();
     
@@ -110,14 +133,17 @@ int main(void)
 
         timer++;
 
-        if (keysHeld() & KEY_UP)    body[0].direction = 0;
-        if (keysHeld() & KEY_DOWN)  body[0].direction = 1;
-        if (keysHeld() & KEY_LEFT)  body[0].direction = 2;
-        if (keysHeld() & KEY_RIGHT) body[0].direction = 3;
+        if (!isGameOver)
+        {
+            if (keysHeld() & KEY_UP)    body[0].direction = 0;
+            if (keysHeld() & KEY_DOWN)  body[0].direction = 1;
+            if (keysHeld() & KEY_LEFT)  body[0].direction = 2;
+            if (keysHeld() & KEY_RIGHT) body[0].direction = 3;
+        }
         
         tailDirection = body[snakeLength-1].direction;
 
-        if (timer % 10 == 0)
+        if (!isGameOver && timer % 10 == 0)
         {
             for (int i = snakeLength - 1; i > 0; i--)
             {
@@ -186,8 +212,8 @@ int main(void)
                 int bobIsOutSnake = 0;
                 while (!bobIsOutSnake)
                 {
-                    bob.x = generateRandomTilePos(14);
-                    bob.y = generateRandomTilePos(9);
+                    bob.x = generateRandomTilePos(14)*16;
+                    bob.y = generateRandomTilePos(9)*16;
 
                     bobIsOutSnake = 1;
                     for (int i = 0; i < snakeLength; i++)
@@ -198,25 +224,67 @@ int main(void)
                         }
                     }
                 }
+                bobSpriteIndex = generateRandomTilePos(1)*12 + 36;
+            }
+
+            for (int i = 1; i < snakeLength; i++)
+            {
+                if (body[0].x == body[i].x && body[0].y == body[i].y)
+                {
+                    isGameOver = 1;
+                    if (snakeLength > maxScore) maxScore = snakeLength;
+                }
+            }
+
+            if (body[0].x >= 240 || body[0].x < 0 || body[0].y >= 160 || body[0].y < 0)
+            {
+                isGameOver = 1;
+                if (snakeLength > maxScore) maxScore = snakeLength;
             }
         }
 
         // Player
         OAM[0].attr0 = (body[0].y & 0xFF) | ATTR0_SQUARE | ATTR0_COLOR_16;
         OAM[0].attr1 = (body[0].x & 0x1FF) | ATTR1_SIZE_16;
-        OAM[0].attr2 = currentSpriteIndex | ATTR2_PALETTE(0);
+        OAM[0].attr2 = currentSpriteIndex | ATTR2_PALETTE(0) | ATTR2_PRIORITY(1);
 
         // Body
         for (int i = 1; i < snakeLength; i++)
         {
             OAM[i].attr0 = (body[i].y & 0xFF) | ATTR0_SQUARE | ATTR0_COLOR_16;
             OAM[i].attr1 = (body[i].x & 0x1FF) | ATTR1_SIZE_16;
-            OAM[i].attr2 = bodyIndex | ATTR2_PALETTE(0);
+            OAM[i].attr2 = bodyIndex | ATTR2_PALETTE(0) | ATTR2_PRIORITY(1);
         }
 
         // Bob
         OAM[127].attr0 = (bob.y & 0xFF) | ATTR0_SQUARE | ATTR0_COLOR_16;
         OAM[127].attr1 = (bob.x & 0x1FF) | ATTR1_SIZE_16;
-        OAM[127].attr2 = bobIndex | ATTR2_PALETTE(0);
+        OAM[127].attr2 = bobSpriteIndex | ATTR2_PALETTE(0) | ATTR2_PRIORITY(1);
+
+        if (!isGameOver)
+        {
+            sprintf(buffer, "Score: %d  ", snakeLength);
+            drawText(1, 1, buffer);
+        }
+        else
+        {
+            sprintf(buffer, "Score: %d Max Score: %d", snakeLength, maxScore);
+            drawText(1, 1, "Game Over!");
+            drawText(1, 2, buffer);
+            if (keysHeld() & KEY_START)
+            {
+                drawText(1, 2, "                                     ");
+                for(int i = 0; i < 128; i++)
+                {
+                    OAM[i].attr0 = ATTR0_DISABLED;
+                    OAM[i].attr1 = 0;
+                    OAM[i].attr2 = 0;
+                }
+                isGameOver = 0;
+                body[0].x = 96; body[0].y = 64; body[0].direction = 2;
+                snakeLength = 1;
+                bob.x = 144; bob.y = 80; bob.direction = 0;
+            } 
+        }
     }
 }
